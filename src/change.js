d44393c807de9ec4f606ad23467ea5727d67d274
Zoe O <zoeo@Zoes-MBP.fios-router.home>
2019-11-03 19:22:22 -0500

    idk

--- src/change.js
+++ src/change.js
@@ -3,12 +3,25 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
-const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
+let amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
-} else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("invalid");
+}
+else if (amount < MIN || amount > MAX) {
+    console.log("invalid");
 } else {
-    // write your code for making change here
+    const quarters = Math.floor(amount/.25);
+    const remainderQuarters = quarters*.25;
+    amount-=remainderQuarters;
+    const dimes = Math.floor(amount/.1);
+    const remainderDimes = dimes*.1;
+    amount-=remainderDimes;
+    const nickels = Math.floor(amount/.05);
+    const remainderNickels = nickels*.05;
+    amount -= remainderNickels;
+    const pennies = Math.ceil(amount/.01);
+    const remainderPennies = pennies*.01;
+    amount -= remainderPennies;
+    console.log("\n"+ quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, and " + pennies + " pennies.\n");
 }
