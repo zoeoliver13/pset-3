e8fd243ff658a2fdb1f8cdcba5d8cfe2500e3f62
Zoe Oliver <ZOliver@ucvts.tec.nj.us>
2019-11-06 10:49:58 -0500

    forgot to save

--- src/months.js
+++ src/months.js
@@ -1,7 +1,7 @@
 const readlineSync = require("readline-sync");
 let month = String(readlineSync.question("\nEnter a month: "));
 month=month.toLowerCase();
-if(month=="january"|| month=="jan"){
+if(month=="january" || month=="jan"){
   console.log("\n31 days.");
 }else if(month=="February"&& month=="Feb"){
     console.log("\n28 or 29 days.");
